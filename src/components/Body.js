b7d2d703722255709d65087e1594bf8de7224223
ArtemKozak666 <artem.kozak.2004@gmail.com>
2025-11-25 17:49:26 +0200

    added information about Artem Kozak

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,10 +4,10 @@ const Body = () => {
   return (
     <main style={styles.body}>
       <div style={styles.card}>
-        <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+        <img src={"/images/artem.png"} alt="Artem Kozak" style={styles.photo} />
         <div style={styles.textContainer}>
-          <h2 style={styles.name}>John Doe</h2>
-          <p style={styles.position}>Software engineer</p>
+          <h2 style={styles.name}>Artem Kozak</h2>
+          <p style={styles.position}>UI/UX designer</p>
         </div>
       </div>
     </main>
