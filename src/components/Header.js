5ce8fc9ee6a16750c14fbf92c1012f4e585a0ccf
yevgenko0 <cfgzys6stw@privaterelay.appleid.com>
2025-12-10 04:47:09 +0200

    Update Header.js
    Changed background color and added fontStyle

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,9 +11,10 @@ const Header = () => {
 const styles = {
   header: {
     padding: "1em",
-    color: "#6a0dad",
+    color: "green",
     textAlign: "center",
     fontWeight: "bold",
+    fontStyle: "italic",
   },
 };
 
